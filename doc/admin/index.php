fd904632fa4314b6f9b4025cdee3eb9346c2ec0b
hankyeol-jung <whd6023@gmail.com>
2022-10-28 17:41:38 +0900

    메뉴 develop

--- doc/admin/index.php
+++ doc/admin/index.php
@@ -24,110 +24,116 @@
             $active="selected";
         }
     ?>
-    <div class='gnb_wrap'>
-        <ul class='menu-tab_group'>
-            <li class='menu-tab <?=$active?>'>
-                <a href='#'>
-                    <i class='fa-solid fa-house'></i>
-                    <span>ADMIN 홈</span>
-                </a>
-            </li>
-            <li class='menu-tab'>
-                <a href='#'>
-                    <i class="fa-solid fa-circle-info"></i>
-                    <span>정보</span>
-                    <button type='button'>
-                        <i class='fa-solid fa-angle-down'></i>
-                    </button>
-                </a>
-                <ul>
-                    <li>
-                        <a href='#'>
-                            <span>홈페이지정보</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href='#'>
-                            <span>회사정보</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href='#'>
-                            <span>링크관리</span>
-                        </a>
-                    </li>
-                </ul>
-            </li>
-            <li class='menu-tab'>
-                <a href='#'>
-                    <i class="fa-solid fa-sitemap"></i>
-                    <span>사이트맵</span>
-                    <button type='button'>
-                        <i class='fa-solid fa-angle-down'></i>
-                    </button>
-                </a>
-                <ul>
-                    <li>
-                        <a href='#'>
-                            <span>메뉴관리</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href='#'>
-                            <span>게시판관리</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href='#'>
-                            <span>페이지관리</span>
-                        </a>
-                    </li>
-                </ul>
-            </li>
-            <li class='menu-tab'>
-                <a href='#'>
-                    <i class="fa-solid fa-user-group"></i>
-                    <span>회원관리</span>
-                </a>
-            </li>
-            <li class='menu-tab'>
-                <a href='#'>
-                    <i class="fa-solid fa-palette"></i>
-                    <span>디자인</span>
-                    <button type='button'>
-                        <i class='fa-solid fa-angle-down'></i>
-                    </button>
-                </a>
-                <ul>
-                    <li>
-                        <a href='#'>
-                            <span>브랜트</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href='#'>
-                            <span>메인</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href='#'>
-                            <span>서브</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href='#'>
-                            <span>팝업</span>
-                        </a>
-                    </li>
-                </ul>
-            </li>
-            <li class='menu-tab'>
-                <a href='#'>
-                    <i class="fa-solid fa-envelopes-bulk"></i>
-                    <span>우편함</span>
-                </a>
-            </li>
-        </ul>
+    <div id='admin_section' class="">
+        <div class='gnb_wrap'>
+            <button class="btn_toggle_gnb" style='display: inline-block;'>
+                <i class="fa-solid fa-arrow-right open_i"></i>
+                <i class="fa-solid fa-arrow-left closed_i"></i>
+            </button>
+            <ul class='gnb'>
+                <li class='menu-tab <?=$active?>'>
+                    <a href='#'>
+                        <i class='fa-solid fa-house'></i>
+                        <span>ADMIN 홈</span>
+                    </a>
+                </li>
+                <li class='menu-tab'>
+                    <a href='#'>
+                        <i class="fa-solid fa-circle-info"></i>
+                        <span>정보</span>
+                        <button type='button'>
+                            <i class='fa-solid fa-angle-down'></i>
+                        </button>
+                    </a>
+                    <ul>
+                        <li>
+                            <a href='#'>
+                                <span>홈페이지정보</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href='#'>
+                                <span>회사정보</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href='#'>
+                                <span>링크관리</span>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class='menu-tab'>
+                    <a href='#'>
+                        <i class="fa-solid fa-sitemap"></i>
+                        <span>사이트맵</span>
+                        <button type='button'>
+                            <i class='fa-solid fa-angle-down'></i>
+                        </button>
+                    </a>
+                    <ul>
+                        <li>
+                            <a href='#'>
+                                <span>메뉴관리</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href='#'>
+                                <span>게시판관리</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href='#'>
+                                <span>페이지관리</span>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class='menu-tab'>
+                    <a href='#'>
+                        <i class="fa-solid fa-user-group"></i>
+                        <span>회원관리</span>
+                    </a>
+                </li>
+                <li class='menu-tab'>
+                    <a href='#'>
+                        <i class="fa-solid fa-palette"></i>
+                        <span>디자인</span>
+                        <button type='button'>
+                            <i class='fa-solid fa-angle-down'></i>
+                        </button>
+                    </a>
+                    <ul>
+                        <li>
+                            <a href='#'>
+                                <span>브랜트</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href='#'>
+                                <span>메인</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href='#'>
+                                <span>서브</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href='#'>
+                                <span>팝업</span>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class='menu-tab'>
+                    <a href='#'>
+                        <i class="fa-solid fa-envelopes-bulk"></i>
+                        <span>우편함</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
     </div>
     
 
--- js/admin.js
+++ js/admin.js
@@ -1,6 +1,11 @@
-// 메뉴 탭 버튼
+// 메뉴
 $(".menu-tab button").on("click", function () {
   $(".menu-tab ul").stop().slideUp();
+  $(".menu-tab").removeClass("expanded");
   $(this).parent().parent().toggleClass("expanded");
   $(this).parent().next().stop().slideToggle();
+});
+
+$(".btn_toggle_gnb").on("click", function () {
+  $("#admin_section").toggleClass("closed");
 });
