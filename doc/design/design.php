6a25b7f943d4a8a1560b1777b22a3f6cfc15060a
hankyeol-jung <whd6023@gmail.com>
2022-10-28 16:17:41 +0900

    버튼 디자인

--- doc/design/design.php
+++ doc/design/design.php
@@ -19,51 +19,75 @@
 </head>
 <body>
 
-    <!-- 메뉴 탭 버튼 -->
-    <ul class='menu-tab_group'>
-        <li class='menu-tab'>
-            <a href='#'>
-                <i class='fa-solid fa-house'></i>
-                <span>ADMIN 홈</span>
-                <button type='button'>
-                    <i class='fa-solid fa-angle-down'></i>
-                </button>
-            </a>
-            <ul>
-                <li>
+    <section class='design'>
+        <div class='width271'>
+            <h5>메뉴 탭</h5>
+            <!-- 메뉴 탭 버튼 -->
+            <ul class='menu-tab_group'>
+                <li class='menu-tab'>
                     <a href='#'>
-                        <span>정보</span>
+                        <i class='fa-solid fa-house'></i>
+                        <span>ADMIN 홈</span>
+                        <button type='button'>
+                            <i class='fa-solid fa-angle-down'></i>
+                        </button>
                     </a>
+                    <ul>
+                        <li>
+                            <a href='#'>
+                                <span>정보</span>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class='menu-tab selected'>
+                    <a href='#'>
+                        <i class='fa-solid fa-house'></i>
+                        <span>ADMIN 홈</span>
+                        <button type='button'>
+                            <i class='fa-solid fa-angle-down'></i>
+                        </button>
+                    </a>
+                    <ul>
+                        <li class='selected'>
+                            <a href='#'>
+                                <span>정보</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href='#'>
+                                <span>정보</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href='#'>
+                                <span>정보</span>
+                            </a>
+                        </li>
+                    </ul>
                 </li>
             </ul>
-        </li>
-        <li class='menu-tab selected'>
-            <a href='#'>
-                <i class='fa-solid fa-house'></i>
-                <span>ADMIN 홈</span>
-                <button type='button'>
-                    <i class='fa-solid fa-angle-down'></i>
-                </button>
-            </a>
-            <ul>
-                <li class='selected'>
-                    <a href='#'>
-                        <span>정보</span>
-                    </a>
-                </li>
-                <li>
-                    <a href='#'>
-                        <span>정보</span>
-                    </a>
-                </li>
-                <li>
-                    <a href='#'>
-                        <span>정보</span>
-                    </a>
-                </li>
-            </ul>
-        </li>
-    </ul>
+        </div>
+        <div>
+            <h5>버튼</h5>
+            <!-- 네모버튼 디자인 -->
+            <div>
+                <a href='#' class='btnSizeM'>글작성</a>
+                <a href='#' class='btnSizeM colorBlueReverse'>a 태그</a>
+                <button type='button' class='btnSizeM colorBlueReverse'>컬러버튼태그</button>
+            </div>
+            <div class='mt-05'>
+                <button type='button' class='btnSizeM colorGrayReverse'>회색버튼태그</button>
+                <button type='button' class='btnSizeM colorRedReverse'>회색버튼태그</button>
+                <a href='#' class='btnSizeS colorGrayReverse custom_btn'>버튼</a>
+            </div>
+            <div class='mt-05'>
+                <a href='#' class='btn_basic_type02'>버튼</a>
+                <a href='#' class='btn_basic_type04'>버튼</a>
+                <a href='#' class='btn_basic_type05'>버튼</a>
+            </div>
+        </div>
+    </section>
 
 </body>
 
--- js/admin.js
+++ js/admin.js
@@ -1,5 +1,6 @@
 // 메뉴 탭 버튼
 $(".menu-tab button").on("click", function () {
+  $(".menu-tab ul").stop().slideUp();
   $(this).parent().parent().toggleClass("expanded");
-  $(this).parent().next().slideToggle();
+  $(this).parent().next().stop().slideToggle();
 });
